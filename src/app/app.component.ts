21122001b6a7a68f1a02c2b6095e1229476c8faf
Joel Tanzi <jtanziredux@gmail.com>
2018-02-23 21:35:21 -0600

    Created text samples and dicationary.json.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dictionary-Based Parser Pipe'
+  textSamples = [
+    'lemon, sliced into 1/4-in slices',
+    'umbrella, neatly closed and dusted',
+    'onion, chopped',
+    'parsley, finely chopped',
+    'parmesean, finely grated',
+    'cheddar cheese, coarsely shredded'
+  ];
 }
