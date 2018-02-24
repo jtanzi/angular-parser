16c755410245036fc22c3ca5f55c15a04aeea19a
Joel Tanzi <jtanziredux@gmail.com>
2018-02-24 13:41:44 -0600

    Set up getWords to return only unique items found in text samples (no duplicate words).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,14 @@ import { Parser } from '../parser';
 export class AppComponent {
   title = 'Dictionary-Based Parser'
   textSamples = [
-    ' lemon, sliced into 1/4-in slices',
+    ' lemons, sliced into 1/4-in slices',
     'umbrella, neatly closed and dusted',
-    'onion, chopped',
+    'onions, chopped',
     'parsley, finely chopped',
     'parmesean, finely grated',
-    'cheddar cheese, coarsely shredded'
+    'cheddar cheese, coarsely shredded',
+    'lemon zest, grated from a fresh lemon',
+    'parsley, washed and minced'
   ];
 
   parser: Parser;
--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
 var json = require('./dictionary.json');
 
+
 export class Parser {
 
     results: any;
@@ -10,10 +11,17 @@ export class Parser {
         this.dictionary = json;
     }
 
+
     getDictionary(): void {
         console.dir(this.dictionary);
     }
 
+    /*
+        name: getWords
+        params: 
+            samples: The array of strings to test against.
+        Returns: An array containing the dictionary values matching the words that have been matched.
+    */
     getWords(samples: string[]): string[] {
         let regexString = '';
         Object.keys(this.dictionary).forEach((key) => {
@@ -29,10 +37,14 @@ export class Parser {
                 console.log(regex.exec(s)[0]);
                 words.push(s.match(regex)[0]);
             }
-            
-            // words.push(s.match(regex));
 
         });
-        return words;
+        const newWords = words.map((word) => {
+            return this.dictionary[word];
+        })
+        const uniqueWords = newWords.filter((elem, index, self) => {
+            return index === self.indexOf(elem);
+        });
+        return uniqueWords;
     }
 }
